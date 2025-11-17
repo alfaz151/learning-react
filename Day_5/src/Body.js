5c1be5248553e1fb01885004104745bd1f35f958
Alfaz Qureshi <alfazqureshi@credenceanalytics.com>
2025-11-18 01:59:38 +0530

    Refactor SearchBar to accept mockJsonData as a prop and update filtering logic

--- Day_5/src/Body.js
+++ Day_5/src/Body.js
@@ -1,12 +1,13 @@
 import { MOCK_JSON_DATA } from "../constant"
 import SearchBar from "./SearchBar"; 
 
+
 const Body = () => {
     return (
         <>
             <div className='main-body'>
                 <div className='body-container'>
-                    <SearchBar />
+                    <SearchBar mockJsonData={MOCK_JSON_DATA} />
                 </div>
             </div>
         </>
--- Day_5/src/SearchBar.js
+++ Day_5/src/SearchBar.js
@@ -1,23 +1,27 @@
-import { useState } from 'react';
-import { MOCK_JSON_DATA } from '../constant';
+import { use, useState } from 'react';
 
 function filterData(searchText, searchlist) {
+    if (!searchText) {
+        return searchlist;
+    }
     return searchlist.filter(data => data.title.toLowerCase().includes(searchText.toLowerCase()));
 }
 
-const SearchBar = () => {
+const SearchBar = ({ mockJsonData }) => {
     const [searchText, setSearchText] = useState("");
-    const [searchList, setSearchList] = useState(MOCK_JSON_DATA);
+    const [searchList, setSearchList] = useState(mockJsonData);
+    const [filteredData, setFilteredData] = useState(mockJsonData);
+
 
     return (
         <div className="search-bar">
             <input type="text" placeholder="Search..." value={searchText} onChange={(e) => setSearchText(e.target.value)} />
             <button onClick={() => {
                 const filteredData = filterData(searchText, searchList);
-                setSearchList(filteredData);
+                setFilteredData(filteredData);
             }}>Search</button>
             {
-                searchList.map(post => {
+                filteredData.map(post => {
                     return (
                         <div className='post-card' key={post.id}>
                             <h2 className='post-title'>{post.title}</h2>
