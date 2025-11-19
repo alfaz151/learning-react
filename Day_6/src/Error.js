3b59f2b742bf4bfba1d69ee9c4566da6d9694ce1
Alfaz Qureshi <alfazqureshi@credenceanalytics.com>
2025-11-20 00:26:39 +0530

    feat: enhance ErrorPage to display dynamic error status and message

--- Day_6/src/Error.js
+++ Day_6/src/Error.js
@@ -1,7 +1,11 @@
+import { useRouteError } from 'react-router-dom';
+
 const ErrorPage = () => {
+    const error = useRouteError();
+    console.error(error);
     return (
         <div>
-            <h1>404 - Page Not Found</h1>
+            <h1>{error.status} - {error.statusText}</h1>
             <p>The page you are looking for does not exist.</p>
         </div>
     );
